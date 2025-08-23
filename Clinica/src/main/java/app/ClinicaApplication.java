5ee452f194e76564f741f4f86e61c459927be51e
TSS <ctoro6960@gmail.com>
2025-08-23 18:41:53 -0500

    initial commit v2

--- Clinica/src/main/java/app/ClinicaApplication.java
+++ Clinica/src/main/java/app/ClinicaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TdeAApplication {
+public class ClinicaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TdeAApplication.class, args);
+		SpringApplication.run(ClinicaApplication.class, args);
 	}
 
 }
